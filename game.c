db177dff5cedfa0002e8b328291f411761e3497b
UH5555 <zhuhao162@163.com>
2022-02-28 20:21:42 +0800

    0228\扫雷All

--- game.c
+++ game.c
@@ -34,3 +34,80 @@ void DisplayBoard(char board[ROWS][COLS], int row, int col)
 		printf("\n");
 	}
 }
+
+void SetMine(char board[ROWS][COLS], int row, int col)
+{
+	int count = EASY_COUNT;
+	while (count)
+	{
+		int x = rand() % row + 1;
+		int y = rand() % col + 1;
+		if (board[x][y] == '0')
+		{
+			board[x][y] = '1';
+			count--;
+		}
+	}
+}
+
+//'1'-'0'=1
+int  get_mine_count(char mine[ROWS][COLS], int x, int y)
+{
+	return mine[x - 1][y] +
+		mine[x - 1][y - 1] +
+		mine[x][y - 1] +
+		mine[x + 1][y - 1] +
+		mine[x + 1][y] +
+		mine[x + 1][y + 1] +
+		mine[x][y + 1] +
+		mine[x - 1][y + 1] - 8 * '0';
+}
+
+void FineMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	int win = 0;
+	again:
+	while (win<row*col-EASY_COUNT)
+	{
+		printf("请输入排查雷的坐标：");
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			//坐标合法
+			//1.踩雷
+			if (mine[x][y] == '1')
+			{
+				printf("很遗憾，你被炸死了！\n");
+				DisplayBoard(mine, row, col);
+				break;
+			}
+			//不是雷
+			else if (show[x][y] != '*')
+			{
+				printf("请重新选择坐标！\n");
+				goto again;
+			}
+			else
+			{
+				//计算x,y坐标作为雷的个数；
+				int count = get_mine_count(mine, x, y);
+				show[x][y] = count + '0';
+				DisplayBoard(show, row, col);
+				win++;
+			}
+		}
+		else
+		{
+			printf("输入坐标非法，请重新输入\n");
+		}
+	}
+	if (win == row * col - EASY_COUNT)
+	{
+		printf("恭喜你排雷成功！\n");
+		DisplayBoard(mine, row, col);
+	}
+}
+
+//展开雷的信息--递归
